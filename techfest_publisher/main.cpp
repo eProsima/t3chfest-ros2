a7f1bf6b0ca58ff4287568be40dc56d240ceeb17
JavierIH <javier.i.h@hotmail.com>
2017-02-09 22:08:27 +0100

    modified format of the examples

--- techfest_publisher/main.cpp
+++ techfest_publisher/main.cpp
@@ -1,52 +1,21 @@
-// Copyright 2016 Open Source Robotics Foundation, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
+#include <iostream>
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
-using namespace std::chrono_literals;
 
-/* This example creates a subclass of Node and uses std::bind() to register a
- * member function as a callback from the timer. */
-
-class MinimalPublisher : public rclcpp::Node
+int main(int argc, char *argv[])
 {
-public:
-  MinimalPublisher()
-  : Node("minimal_publisher"), count_(0)
-  {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("topic");
-    timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
-  }
-
-private:
-  void timer_callback()
-  {
-    auto message = std_msgs::msg::String();
-    message.data = "Hello, world! " + std::to_string(count_++);
-    printf("Publisfhing: [%s]\n", message.data.c_str());
-    publisher_->publish(message);
-  }
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-  size_t count_;
-};
-
-int main(int argc, char * argv[])
-{
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
-  return 0;
+    rclcpp::init(argc, argv);
+    auto node = rclcpp::node::Node::make_shared("talker_node");
+    auto publisher = node->create_publisher<std_msgs::msg::String>("topic");
+    auto message = std::make_shared<std_msgs::msg::String>();
+    auto publish_count = 0;
+    while (rclcpp::ok()) {
+        message->data = "Hello, world! " + std::to_string(publish_count++);
+        printf("> Publishing: [%s]\n", message->data.c_str());
+        publisher->publish(message);
+        rclcpp::spin_some(node);
+        rclcpp::utilities::sleep_for(std::chrono::milliseconds(500));
+    }
+    return 0;
 }
